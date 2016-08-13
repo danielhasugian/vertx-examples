046a71b00baa7167eee75e986e7078da326eca72
daniel.daniel <daniel.daniel@PJI-00074.jatis.com>
2016-08-14 01:25:38 +0700

    Update configuration for postgreSql

--- src/main/java/com/organization/project/controller/Core.java
+++ src/main/java/com/organization/project/controller/Core.java
@@ -1,47 +1,94 @@
 package com.organization.project.controller;
 
+import com.organization.project.service.PostgreSqlService;
 import com.organization.project.util.Runner;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 
 public class Core extends AbstractVerticle {
 
 	private static final int LISTENED_PORT = 88;
-	private static final String CONTENT_TYPE[] = {"text/html","application/json"};
+	private static final int MAX_INITIAL_LINE_LENGTH = 16384;
+	private static final String CONTENT_TYPE[] = { "text/html", "application/json" };
 	private Router router;
 	private EmployeController employeController;
+	private UserController userController;
 	
+	private PostgreSqlService postgreSqlService;
+	private Vertx vertx;
+	private JDBCClient jdbc;
+
 	public static void main(String[] args) {
 		Runner.runExample(Core.class);
 	}
 
 	@Override
-	public void start() {
+	public void start(Future<Void> fut) {
+		// initial core
+		initialCore();
+
 		router = Router.router(vertx);
-		employeController = new EmployeController();
+		//handleService
+		handleService();
+		jdbc = postgreSqlService.setUp(vertx, "createShared");
 		
+		handleController();
+
 		router.route().handler(BodyHandler.create());
 		router.get("/").handler(routingContext -> {
 			routingContext.response().putHeader("content-type", CONTENT_TYPE[0]).end(getMessage());
 		});
-		
-		//initial postgreSql
-		
-		//Setup EmployeController
+
+		// Setup EmployeController
 		employeController.setUpInitialData();
 		router.get("/employees").handler(employeController::handleListEmployees);
 		router.post("/employees/clear").handler(employeController::handleClearEmployees);
 		router.post("/employees/setup").handler(employeController::handleSetUpInitialData);
-		router.post("/employees/:employeeId").handler(employeController::handleGetEmployee);
+		router.get("/employees/:employeeId").handler(employeController::handleGetEmployee);
 		router.post("/employees/:employeeId").handler(employeController::handleAddEmployee);
-		vertx.createHttpServer().requestHandler(router::accept).listen(LISTENED_PORT);
+		
+		//Setup User
+		router.get("/user/sampling").handler(userController::getDataExample);
+
+		vertx.createHttpServer(serverOption()).requestHandler(router::accept)
+				.listen(config().getInteger("http.port", LISTENED_PORT), result -> {
+					if (result.succeeded()) {
+						fut.complete();
+					} else {
+						fut.fail(result.cause());
+					}
+				});
 	}
 
-	public String getMessage(){
-		return "<center><h1 style='color:blue;'>Vertext Example Project</h1></center><br/>"
-				+ "<center><h2>Under Construction</h2></center>";
+	private String getMessage() {
+		return "<center>" + "<h1 style='color:blue;'>" + "Micropayment Project" + "</h1>" + "</center>";
 	}
 
+	private HttpServerOptions serverOption() {
+		HttpServerOptions opt = new HttpServerOptions();
+		opt.setMaxInitialLineLength(MAX_INITIAL_LINE_LENGTH);
+		return opt;
+	}
+
+	private void initialCore() {
+		VertxOptions options = new VertxOptions();
+		options.setMaxEventLoopExecuteTime(Long.MAX_VALUE);
+		this.vertx = Vertx.vertx(options);
+	}
+
+	private void handleService() {
+		postgreSqlService = new PostgreSqlService();
+	}
+
+	private void handleController() {
+		employeController = new EmployeController();
+		userController = new UserController(jdbc);
+	}
 }
--- src/test/java/com/jatis/project/controller/CoreTest.java
+++
@@ -1,5 +0,0 @@
-package com.jatis.project.controller;
-
-public class CoreTest {
-	
-}
